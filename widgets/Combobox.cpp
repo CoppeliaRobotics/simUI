8c71a786577d1ea42d41d7f9f30e13bbf8b68bf7
Federico Ferri <federico.ferri.it@gmail.com>
2016-04-13 19:28:02 +0200

    remove some DEBUG sections

--- widgets/Combobox.cpp
+++ widgets/Combobox.cpp
@@ -29,15 +29,9 @@ bool Combobox::parse(tinyxml2::XMLElement *e, std::vector<std::string>& errors)
     for(tinyxml2::XMLElement *e1 = e->FirstChildElement(); e1; e1 = e1->NextSiblingElement())
     {
         std::string tag1(e1->Value());
-#ifdef DEBUG
-        std::cerr << "combobox: children element: <" << tag1 << ">" << std::endl;
-#endif
         if(tag1 != "item") continue;
         std::string itemName(e1->GetText());
         items.push_back(itemName);
-#ifdef DEBUG
-        std::cerr << "combobox: add item: " << itemName << std::endl;
-#endif
     }    
 
     if(e->Attribute("onchange")) onchange = e->Attribute("onchange");
