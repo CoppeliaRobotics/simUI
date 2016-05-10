bd30e54978f6274cc4125b4f67f4cb4b4b568df8
Federico Ferri <federico.ferri.it@gmail.com>
2016-05-10 04:54:48 +0200

    forgot one QObject connection

--- UIFunctions.cpp
+++ UIFunctions.cpp
@@ -24,6 +24,7 @@ UIFunctions::UIFunctions(QObject *parent)
     connect(this, SIGNAL(destroyUi(Window*)), uiproxy, SLOT(onDestroyUi(Window*)));
     connect(this, SIGNAL(showWindow(Window*)), uiproxy, SLOT(onShowWindow(Window*)));
     connect(this, SIGNAL(hideWindow(Window*)), uiproxy, SLOT(onHideWindow(Window*)));
+    connect(this, SIGNAL(setImage(Image*,const char*,int,int)), uiproxy, SLOT(onSetImage(Image*,const char*,int,int)));
     connect(this, SIGNAL(sceneChange(Window*,int,int)), uiproxy, SLOT(onSceneChange(Window*,int,int)));
 }
 
