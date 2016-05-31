2f185d014098980c54ce8e4861adce7322b6504b
Federico Ferri <federico.ferri.it@gmail.com>
2016-05-31 16:05:29 +0200

    add all widgets header file

---
+++ widgets/all.h
@@ -0,0 +1,20 @@
+#include "Widget.h"
+#include "Window.h"
+
+#include "Event.h"
+
+#include "Button.h"
+#include "Checkbox.h"
+#include "Combobox.h"
+#include "Edit.h"
+#include "Group.h"
+#include "HSlider.h"
+#include "Image.h"
+#include "Label.h"
+#include "LayoutWidget.h"
+#include "Radiobutton.h"
+#include "Slider.h"
+#include "Spinbox.h"
+#include "Tabs.h"
+#include "VSlider.h"
+
