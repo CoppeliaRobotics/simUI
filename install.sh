453a2e6587036d43dfa861cf50a205e105b7f273
Federico Ferri <federico.ferri.it@gmail.com>
2016-10-06 17:32:56 +0200

    add build_target var

--- install.sh
+++ install.sh
@@ -6,13 +6,17 @@ if [ "x$VREP_ROOT" = "x" ]; then
 fi
 
 if [ "`uname`" = "Darwin" ]; then
-    TARGET="$VREP_ROOT/vrep.app/Contents/MacOS/"
+    INSTALL_TARGET="$VREP_ROOT/vrep.app/Contents/MacOS/"
     DLEXT=dylib
 else
-    TARGET="$VREP_ROOT"
+    INSTALL_TARGET="$VREP_ROOT"
     DLEXT=so
 fi
 
-make && \
-cp -v "$(dirname "$0")/libv_repExtCustomUI.$DLEXT" "$TARGET"
+if [ "x$BUILD_TARGET" = "x" ]; then
+    BUILD_TARGET=debug
+fi
+
+make $BUILD_TARGET && \
+cp -v "`dirname "$0"`/libv_repExtCustomUI.$DLEXT" "$INSTALL_TARGET"
 
