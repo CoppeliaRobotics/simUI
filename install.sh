9564dc575755ca756956ae35bf01929dfe51efd3
Federico Ferri <federico.ferri.it@gmail.com>
2016-10-06 17:20:41 +0200

    add install script for *nix

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "x$VREP_ROOT" = "x" ]; then
+    echo "error: \$VREP_ROOT is not set" 1>&2
+    exit 1
+fi
+
+if [ "`uname`" = "Darwin" ]; then
+    TARGET="$VREP_ROOT/vrep.app/Contents/MacOS/"
+    DLEXT=dylib
+else
+    TARGET="$VREP_ROOT"
+    DLEXT=so
+fi
+
+cp -v "$(dirname "$0")/libv_repExtCustomUI.$DLEXT" "$TARGET"
+
